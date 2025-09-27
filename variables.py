990c31b618fdcb2a4498b310d48680a40d27b98b
codybuchholz <159822536+codybuchholz@users.noreply.github.com>
2025-09-27 13:04:38 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+kg_value_1 = 21
+kg_value_2 = 0.5
+kg_value_3 = 40
+kg_value_4 = 10
+
+conversion_factor = 2.20462
+
+lb_value_1 = kg_value_1 / conversion_factor
+lb_value_2 = kg_value_2 / conversion_factor
+lb_value_3 = kg_value_3 / conversion_factor
+lb_value_4 = kg_value_4 / conversion_factor
+print(f"{kg_value_1} kilograms is equal to {lb_value_1:.2f} pounds")
+print(f"{kg_value_2} kilograms is equal to {lb_value_2:.2f} pounds")
+print(f"{kg_value_3} kilograms is equal to {lb_value_3:.2f} pounds")
+print(f"{kg_value_4} kilograms is equal to {lb_value_4:.2f} pounds")
