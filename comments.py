2d16a89ec47599c26082e05068d4ae640cba5b0b
codybuchholz <159822536+codybuchholz@users.noreply.github.com>
2025-09-27 14:25:28 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+#this is a comment
+'''this is a multi-line comment'''
+tank_size = 15
+# Define the total distance covered
+distance = 600
+#gallons are the unit of measurment that 600 relates too.
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+# TODO: This function needs work
+print("Miles per gallon:", mpg)
