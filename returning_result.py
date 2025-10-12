0d602799a2e184d856ba24cf2bbe651f9e80dc6d
codybuchholz <159822536+codybuchholz@users.noreply.github.com>
2025-10-12 15:59:41 -0500

    Add files via upload

---
+++ returning_result.py
@@ -0,0 +1,12 @@
+def calculate_interest(principal, rate, time):
+    simple_interest = (principal * rate * time) / 100
+    return simple_interest
+
+def main():
+    principal = float(input("Enter the principal amount: "))
+    rate = float(input("Enter the interest rate: "))
+    time = float(input("Enter the time in years: "))
+    interest = calculate_interest(principal, rate, time)
+
+    print(f"The simple interest for ${principal:,.2f} at {rate}% over {time} years is ${interest:,.2f}.")
+main()
