784119359088c2463256958331ba36478a4f3570
codybuchholz <159822536+codybuchholz@users.noreply.github.com>
2025-10-30 19:29:50 -0500

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,30 @@
+string1 = "python"
+print(string1.capitalize())
+
+string2 = "python"
+print(string2.center(10))
+
+string3 = "python"
+print(string3.endswith("on"))
+
+string4 = "python"
+print(string4.find("th"))
+
+string5 = "python3"
+print(string5.isalnum())
+
+string6 = "python"
+print(string6.isalpha())
+
+string7 = "python"
+print(string7.islower())
+
+string8 = " "
+print(string8.isspace())
+
+string9 = "PYTHON"
+print(string9.isupper())
+
+iterable1 = ["Python", "is", "fun"]
+separator = "-"
+print(separator.join(iterable1))
